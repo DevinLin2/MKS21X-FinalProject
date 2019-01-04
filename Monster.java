55f02fba35b76a09c109f5c6f8bfff9e78ec6b9a
Jack Lam <jlam00@stuy.edu>
2019-01-04 10:57:15 -0500

    added x cord and y cord

--- Monster.java
+++ Monster.java
@@ -1,13 +1,14 @@
 public class Monster implements Damageable{
 private int health;
 private int damage;
+private int x;
+private int y;
 
 public void attack(){
 
 }
-public void takeDamage(int damage){
-
-
+public void takeDamage(int damage){ // might make it return int
+health = health - damage;
 }
 
 public void move(){
