c68339d6cb2ce8c7672cd1bd40684f5658a3c7f0
Jack Lam <jlam00@stuy.edu>
2019-01-04 10:47:48 -0500

    created class monster anddid getDamage() and getHealth()

---
+++ Monster.java
@@ -0,0 +1,25 @@
+public class Monster implements Damageable{
+private int health;
+private int damage;
+
+public void attack(){
+
+}
+public void takeDamage(int damage){
+
+
+}
+
+public void move(){
+
+}
+
+public int getDamage(){
+return damage;
+}
+
+public int getHealth(){
+  return health;
+}
+
+}
