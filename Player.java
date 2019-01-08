bf4f2e181124a667a2e686966335a5e01ee0fe92
VitaminOh <dlin01@stuy.edu>
2019-01-08 10:56:34 -0500

    made constructor for player and the move function

--- Player.java
+++ Player.java
@@ -1,9 +1,37 @@
 public class Player{
   private int health;
-  private int x;
-  private int y;
-  public Player(){
+  private int xCoord;
+  private int yCoord;
+  private char logo;
+  private int damage;
+  public Player(int h, int x, int y, int d){
+    health = h;
+    xCoord = x;
+    yCoord = y;
+    logo = '\u0b87';
+    damage = d;
   }
-  public void takeDamage(int damage){
+  public void move(String direction){
+    if (direction.equals("up")){
+      yCoord--;
+    }
+    if (direction.equals("down")){
+      yCoord++;
+    }
+    if (direction.equals("left")){
+      xCoord++;
+    }
+    if (direction.equals("right")){
+      xCoord--;
+    }
+  }
+  public char getCharacter(){
+    return logo;
+  }
+  public int getX(){
+    return xCoord;
+  }
+  public int getY(){
+    return yCoord;
   }
 }
