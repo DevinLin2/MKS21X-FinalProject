e892f941645077a6de6f4da1185f82fb8e22892e
VitaminOh <dlin01@stuy.edu>
2019-01-09 11:04:37 -0500

    updated to use screen

--- Field.java
+++ Field.java
@@ -11,13 +11,17 @@ import com.googlecode.lanterna.input.InputDecoder;
 import com.googlecode.lanterna.input.InputProvider;
 import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.input.KeyMappingProfile;
+import com.googlecode.lanterna.screen.Screen;
 
 public class Field{
   public static void main(String[] args) {
-    Terminal terminal = TerminalFacade.createTextTerminal();
-    terminal.enterPrivateMode();
-    boolean running = true;
     Player bob = new Player(100, 10, 10, 2);
+    Terminal terminal = TerminalFacade.createTextTerminal();
+    Screen screen = new Screen(terminal);
+    screen.startScreen();
+    screen.putString(1,3,"Health: " + bob.getHealth(), Terminal.Color.WHITE,Terminal.Color.RED);
+    screen.refresh();
+    boolean running = true;
     while (running){
       terminal.moveCursor(bob.getX(),bob.getY());
       terminal.putCharacter(bob.getCharacter());
@@ -25,7 +29,7 @@ public class Field{
       terminal.setCursorVisible(false);
       if (key != null){
         if (key.getKind() == Key.Kind.Escape){
-          terminal.exitPrivateMode();
+          screen.stopScreen();
           running = false;
         }
         if (key.getKind() == Key.Kind.ArrowUp){
