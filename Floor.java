0386ced5acc61d65cecc9cf04ed60b7ce1991aa5
Devin Lin <Dlin01@stuy.edu>
2019-01-12 08:41:05 -0500

    made constructor and added function to add to arrayList

--- Floor.java
+++ Floor.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
 public class Floor{
-  private Wall[][] border;
-  public Floor(int row, int col){
-    border = new
+  /**
+   * ArrayList of Walls which maps the playing field.
+   */
+  private ArrayList<Wall> border;
+  /**
+   * The level the player is currently on. This is the identifier for the floor.
+   */
+  private int level;
+  /**
+   * Constructs and empty floor.
+   * @param Level The level of the floor.
+   */
+  public Floor(int Level){
+    border = new ArrayList<Wall>();
+    level = Level;
+  }
+  /**
+   * Constructs a new wall and adds it to the ArrayList of walls.
+   * @param x X coordinate for wall.
+   * @param y Y coordinate for wall.
+   */
+  public void addWall(int x, int y){
+    Wall toAdd = new Wall(x,y);
+    border.add(toAdd);
   }
 }
