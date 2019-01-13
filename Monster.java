1579c5a04844257bf96f5d4c827132ecb67814f9
Devin Lin <Dlin01@stuy.edu>
2019-01-13 15:16:54 -0500

    cleaned up code

--- Monster.java
+++ Monster.java
@@ -1,49 +1,50 @@
 public class Monster implements Damageable{
-private int health;
-private int damage;
-private int x;
-private int y;
-private char logo;
-logo = '\u2639';
-
-public Monster(int h,int xcord, int ycord, int d){
-  health = h;
-  damage = d;
-  x = xcord;
-  y = ycord;
-}
-public char getCharacter(){
-  return logo;
-}
-public int attack(){
-return damage;
-}
-public void takeDamage(int damage){ // might make it return int
-health = health - damage;
-}
-
-public void move(String direction){
-  if (direction.equals("up")){
-    y--;
+  private int health;
+  private int damage;
+  private int x;
+  private int y;
+  private char logo;
+  public Monster(int h,int xcord, int ycord, int d){
+    health = h;
+    damage = d;
+    x = xcord;
+    y = ycord;
+    logo = '\u2639';
   }
-  if (direction.equals("down")){
-    y++;
+  public char getCharacter(){
+    return logo;
   }
-  if (direction.equals("left")){
-    x++;
+  /**
+   * Deducts health from the Monster
+   * @param damage The amount of health to Deduct
+   */
+  public void takeDamage(int damage){
+    health = health - damage;
   }
-  if (direction.equals("right")){
-    x--;
+  public int getDamage(){
+    return damage;
   }
-}
-
-public int getDamage(){
-return damage;
-}
-
-public int getHealth(){
-  return health;
-}
-
-
+  public int getHealth(){
+    return health;
+  }
+  public int getX(){
+    return x;
+  }
+  public int getY(){
+    return y;
+  }
+  public void move(String direction){
+    if (direction.equals("up")){
+      y--;
+    }
+    if (direction.equals("down")){
+      y++;
+    }
+    if (direction.equals("left")){
+      x++;
+    }
+    if (direction.equals("right")){
+      x--;
+    }
+  }
 }
