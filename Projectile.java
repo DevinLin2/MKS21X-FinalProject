c6311ae0ab3fb1a0443947d92966aaba09635ffb
Devin Lin <Dlin01@stuy.edu>
2019-01-19 18:23:39 -0500

    completed framework for projectile; might need some additions

---
+++ Projectile.java
@@ -0,0 +1,40 @@
+public class Projectile {
+  private int x;
+  private int y;
+  private String direction;
+  private char logo;
+  private int damage;
+  public Projectile(int xCoord, int yCoord, int dam, String d){
+    x = xCoord;
+    y = yCoord;
+    direction = d;
+    damage = dam;
+    logo = '\u25E6';
+  }
+  public void move(){
+    if (direction.equals("up")){
+      y--;
+    }
+    if (direction.equals("down")){
+      y++;
+    }
+    if (direction.equals("left")){
+      x--;
+    }
+    if (direction.equals("right")){
+      x++;
+    }
+  }
+  public int getX(){
+    return x;
+  }
+  public int getY(){
+    return y;
+  }
+  public int getDamage(){
+    return damage;
+  }
+  public char getLogo(){
+    return logo;
+  }
+}
